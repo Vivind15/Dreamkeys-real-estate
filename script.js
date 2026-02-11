34479917fc53651bbb187ecb4af54305fb2355e0
Vivind15 <vivinddelekar08@gmail.com>
2026-02-11 18:14:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("search").addEventListener("keyup", function() {
+    let value = this.value.toLowerCase();
+    let properties = document.querySelectorAll(".property");
+
+    properties.forEach(function(property) {
+        let city = property.getAttribute("data-city").toLowerCase();
+        if (city.includes(value)) {
+            property.style.display = "inline-block";
+        } else {
+            property.style.display = "none";
+        }
+    });
+});
